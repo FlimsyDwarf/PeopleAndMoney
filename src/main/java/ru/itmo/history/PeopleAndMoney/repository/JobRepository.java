d98eb7bc62d31803b73188c04d2d8a9190f7a6ea
Zagrebin_Grigorii <grigorii.zagr@gmail.com>
2023-12-23 10:19:17 +0300

    files deleted

--- src/main/java/ru/itmo/history/PeopleAndMoney/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.itmo.history.PeopleAndMoney.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.itmo.history.PeopleAndMoney.domain.Job;
-
-import java.util.List;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
